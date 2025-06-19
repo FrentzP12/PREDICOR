740b2498ce51e764ef30fc92851bfdace3b00a7f
Frentz Diego Padilla Gonzales <frentz233@gmail.com>
2025-06-19 00:04:23 -0500

    a

--- project/settings.py
+++ project/settings.py
@@ -74,8 +74,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
